1191dc9435b7a37476861340dd40d5c33e3ce151
genkokudo <y-toyoda@live.jp>
2024-06-30 11:02:49 +0900

    CIてすと

--- BliFunc/Function1.cs
+++ BliFunc/Function1.cs
@@ -39,5 +39,18 @@ namespace BliFunc
 
             return response;
         }
+
+        [Function("Function3")]
+        public HttpResponseData Run3([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Ç»ÇÒÇ∆3Ç¬ÇﬂÇÃä÷êî");
+
+            return response;
+        }
     }
 }
