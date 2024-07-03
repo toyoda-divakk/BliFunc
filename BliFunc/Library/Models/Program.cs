409362ed2ee1c09971e36fd08a74c7eba305aefa
genkokudo <y-toyoda@live.jp>
2024-07-03 23:09:40 +0900

    CosmosDBについて

--- BliFunc/Library/Models/Program.cs
+++ BliFunc/Library/Models/Program.cs
@@ -7,35 +7,35 @@
 
 //namespace BliFunc.Library.Models
 //{
+//    // んー？データベースがあって、その中にパーティションキーで名づけられたコンテナが複数あって、その中にパーティションキーで名づけられたアイテムが複数ある？
+
 //    class Program
 //    {
-//        // The Azure Cosmos DB endpoint for running this sample.
-//        private static readonly string EndpointUri = ConfigurationManager.AppSettings["EndPointUri"];
+//        // エンドポイント
+//        private static readonly string EndpointUri = ConfigurationManager.AppSettings["EndPointUri"];       // 環境変数からとること
 
-//        // The primary key for the Azure Cosmos account.
-//        private static readonly string PrimaryKey = ConfigurationManager.AppSettings["PrimaryKey"];
+//        // なんかものすごい文字列が入る
+//        private static readonly string PrimaryKey = ConfigurationManager.AppSettings["PrimaryKey"];       // 環境変数からとること
 
-//        // The Cosmos client instance
+//        // クライアント
 //        private CosmosClient cosmosClient;
 
-//        // The database we will create
+//        // DB本体
 //        private Database database;
 
-//        // The container we will create.
+//        // コンテナ（って何？）
 //        private Container container;
 
-//        // The name of the database and container we will create
+//        // 作成するデータベースとコンテナの名前。
 //        private string databaseId = "ToDoList";
 //        private string containerId = "Items";
 
-//        // <Main>
 //        public static async Task Main(string[] args)
 //        {
 //            try
 //            {
-//                Console.WriteLine("Beginning operations...\n");
-//                Program p = new Program();
-//                await p.GetStartedDemoAsync();
+//                var p = new Program();
+//                await p.GetStartedDemoAsync();  // cosmosClientに繋いでいろいろ作成、実行
 
 //            }
 //            catch (CosmosException de)
@@ -53,62 +53,53 @@
 //                Console.ReadKey();
 //            }
 //        }
-//        // </Main>
 
-//        // <GetStartedDemoAsync>
 //        /// <summary>
-//        /// Entry point to call methods that operate on Azure Cosmos DB resources in this sample
+//        /// このサンプルで Azure Cosmos DB リソースを操作するメソッドを呼び出すエントリポイント
 //        /// </summary>
 //        public async Task GetStartedDemoAsync()
 //        {
-//            // Create a new instance of the Cosmos Client
-//            cosmosClient = new CosmosClient(EndpointUri, PrimaryKey, new CosmosClientOptions() { ApplicationName = "CosmosDBDotnetQuickstart" });
-//            await CreateDatabaseAsync();
-//            await CreateContainerAsync();
-//            await ScaleContainerAsync();
-//            await AddItemsToContainerAsync();
-//            await QueryItemsAsync();
-//            await ReplaceFamilyItemAsync();
-//            await DeleteFamilyItemAsync();
-//            await DeleteDatabaseAndCleanupAsync();
+//            // Cosmosクライアントの新しいインスタンスを作成します。
+//            cosmosClient = new CosmosClient(EndpointUri, PrimaryKey, new CosmosClientOptions() { ApplicationName = "CosmosDBDotnetQuickstart" });   // ApplicationNameとは？
+//            await CreateDatabaseAsync();    // データベースが存在しない場合は作成
+//            await CreateContainerAsync();   // コンテナが存在しない場合は作成（パーティション・キーを入れてリクエストと保存の分散とかいうことをやってる）
+//            await ScaleContainerAsync();    // コンテナのスループットをスケール（なにそれ）
+//            await AddItemsToContainerAsync();   // ファミリーのアイテムをコンテナに追加する、無かったら作成、あったら何もしない
+//            await QueryItemsAsync();                // クエリの実行。キーはパーティションキーを使っている（じゃあIDは？）
+//            await ReplaceFamilyItemAsync(); // コンテナ内のアイテムを置き換える（更新する）
+//            await DeleteFamilyItemAsync();  // コンテナ内の項目を削除する
+//            await DeleteDatabaseAndCleanupAsync();  // データベースを削除し、Cosmosクライアントインスタンスを破棄
 //        }
-//        // </GetStartedDemoAsync>
 
-//        // <CreateDatabaseAsync>
 //        /// <summary>
-//        /// Create the database if it does not exist
+//        /// データベースが存在しない場合は作成する
 //        /// </summary>
 //        private async Task CreateDatabaseAsync()
 //        {
-//            // Create a new database
+//            // 新しいデータベースを作成する
 //            database = await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
 //            Console.WriteLine("Created Database: {0}\n", database.Id);
 //        }
-//        // </CreateDatabaseAsync>
 
-//        // <CreateContainerAsync>
 //        /// <summary>
-//        /// Create the container if it does not exist. 
-//        /// Specifiy "/partitionKey" as the partition key path since we're storing family information, to ensure good distribution of requests and storage.
+//        /// コンテナが存在しない場合は作成する。
+//        /// ファミリー情報を保存するため、パーティション・キー・パスとして"/partitionKey "を指定し、リクエストと保存の分散を図る。（どういうこと？一時領域みたいな？）
 //        /// </summary>
 //        /// <returns></returns>
 //        private async Task CreateContainerAsync()
 //        {
-//            // Create a new container
 //            container = await database.CreateContainerIfNotExistsAsync(containerId, "/partitionKey");
 //            Console.WriteLine("Created Container: {0}\n", container.Id);
 //        }
-//        // </CreateContainerAsync>
 
-//        // <ScaleContainerAsync>
 //        /// <summary>
-//        /// Scale the throughput provisioned on an existing Container.
-//        /// You can scale the throughput (RU/s) of your container up and down to meet the needs of the workload. Learn more: https://aka.ms/cosmos-request-units
+//        /// 既存のコンテナにプロビジョニングされたスループットをスケールします。
+//        /// ワークロードのニーズに合わせて、コンテナのスループット（RU/s）を上下させることができる。 Learn more: https://aka.ms/cosmos-request-units
 //        /// </summary>
 //        /// <returns></returns>
 //        private async Task ScaleContainerAsync()
 //        {
-//            // Read the current throughput
+//            // 現在のスループットを読む
 //            try
 //            {
 //                int? throughput = await container.ReadThroughputAsync();
@@ -116,7 +107,7 @@
 //                {
 //                    Console.WriteLine("Current provisioned throughput : {0}\n", throughput.Value);
 //                    int newThroughput = throughput.Value + 100;
-//                    // Update throughput
+//                    // スループットの更新
 //                    await container.ReplaceThroughputAsync(newThroughput);
 //                    Console.WriteLine("New provisioned throughput : {0}\n", newThroughput);
 //                }
@@ -128,81 +119,79 @@
 //            }
 
 //        }
-//        // </ScaleContainerAsync>
 
-//        // <AddItemsToContainerAsync>
 //        /// <summary>
-//        /// Add Family items to the container
+//        /// ファミリーのアイテムをコンテナに追加する
 //        /// </summary>
 //        private async Task AddItemsToContainerAsync()
 //        {
-//            // Create a family object for the Andersen family
-//            Family andersenFamily = new Family
+//            // アンデルセン家のファミリーオブジェクトを作成する
+//            var andersenFamily = new Family
 //            {
 //                Id = "Andersen.1",
-//                PartitionKey = "Andersen",
+//                PartitionKey = "Andersen",          // パーティションキー（ってなんだろう）
 //                LastName = "Andersen",
-//                Parents = new Parent[]
-//                {
+//                Parents =
+//                [
 //                    new Parent { FirstName = "Thomas" },
 //                    new Parent { FirstName = "Mary Kay" }
-//                },
-//                Children = new Child[]
-//                {
+//                ],
+//                Children =
+//                [
 //                    new Child
 //                    {
 //                        FirstName = "Henriette Thaulow",
 //                        Gender = "female",
 //                        Grade = 5,
-//                        Pets = new Pet[]
-//                        {
+//                        Pets =
+//                        [
 //                            new Pet { GivenName = "Fluffy" }
-//                        }
+//                        ]
 //                    }
-//                },
+//                ],
 //                Address = new Address { State = "WA", County = "King", City = "Seattle" },
 //                IsRegistered = false
 //            };
 
 //            try
 //            {
-//                // Read the item to see if it exists.  
-//                ItemResponse<Family> andersenFamilyResponse = await container.ReadItemAsync<Family>(andersenFamily.Id, new PartitionKey(andersenFamily.PartitionKey));
+//                // その項目が存在するかどうかを読む。
+//                ItemResponse<Family> andersenFamilyResponse = await container.ReadItemAsync<Family>(andersenFamily.Id, new PartitionKey(andersenFamily.PartitionKey));      // パーティションキーってなに？※見つからなかったら例外に飛ぶそうな
 //                Console.WriteLine("Item in database with id: {0} already exists\n", andersenFamilyResponse.Resource.Id);
 //            }
 //            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
 //            {
-//                // Create an item in the container representing the Andersen family. Note we provide the value of the partition key for this item, which is "Andersen"
+//                // Andersenファミリーを表すアイテムをコンテナに作成する。このアイテムのパーティション・キーの値は "Andersen"です。
 //                ItemResponse<Family> andersenFamilyResponse = await container.CreateItemAsync(andersenFamily, new PartitionKey(andersenFamily.PartitionKey));
 
-//                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
+//                // アイテムを作成した後、ItemResponse の Resource プロパティでアイテムのボディにアクセスできることに注意してください。また、RequestChargeプロパティにアクセスして、このリクエストで消費されたRUの量を確認することもできます。
 //                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", andersenFamilyResponse.Resource.Id, andersenFamilyResponse.RequestCharge);
 //            }
 
-//            // Create a family object for the Wakefield family
-//            Family wakefieldFamily = new Family
+//            // ウェイクフィールド家のファミリーオブジェクトを作成する
+//            var wakefieldFamily = new Family
 //            {
 //                Id = "Wakefield.7",
 //                PartitionKey = "Wakefield",
 //                LastName = "Wakefield",
-//                Parents = new Parent[]
-//                {
+//                Parents =
+//                [
 //                    new Parent { FamilyName = "Wakefield", FirstName = "Robin" },
 //                    new Parent { FamilyName = "Miller", FirstName = "Ben" }
-//                },
-//                Children = new Child[]
-//                {
+//                ],
+//                Children =
+//                [
 //                    new Child
 //                    {
 //                        FamilyName = "Merriam",
 //                        FirstName = "Jesse",
 //                        Gender = "female",
 //                        Grade = 8,
-//                        Pets = new Pet[]
-//                        {
+//                        Pets =
+//                        [
 //                            new Pet { GivenName = "Goofy" },
 //                            new Pet { GivenName = "Shadow" }
-//                        }
+//                        ]
 //                    },
 //                    new Child
 //                    {
@@ -211,32 +200,29 @@
 //                        Gender = "female",
 //                        Grade = 1
 //                    }
-//                },
+//                ],
 //                Address = new Address { State = "NY", County = "Manhattan", City = "NY" },
 //                IsRegistered = true
 //            };
 
 //            try
 //            {
-//                // Read the item to see if it exists
+//                // アイテムが存在するかどうかを読み取る
 //                ItemResponse<Family> wakefieldFamilyResponse = await container.ReadItemAsync<Family>(wakefieldFamily.Id, new PartitionKey(wakefieldFamily.PartitionKey));
 //                Console.WriteLine("Item in database with id: {0} already exists\n", wakefieldFamilyResponse.Resource.Id);
 //            }
 //            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
 //            {
-//                // Create an item in the container representing the Wakefield family. Note we provide the value of the partition key for this item, which is "Wakefield"
+//                // Wakefieldファミリーを表すアイテムをコンテナ内に作成する。このアイテムのパーティション・キーの値は "Wakefield"である。
 //                ItemResponse<Family> wakefieldFamilyResponse = await container.CreateItemAsync(wakefieldFamily, new PartitionKey(wakefieldFamily.PartitionKey));
 
-//                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
+//                // アイテムを作成した後、ItemResponse の Resource プロパティでアイテムのボディにアクセスできることに注意してください。また、RequestChargeプロパティにアクセスして、このリクエストで消費されたRUの量を確認することもできます。
 //                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", wakefieldFamilyResponse.Resource.Id, wakefieldFamilyResponse.RequestCharge);
 //            }
 //        }
-//        // </AddItemsToContainerAsync>
 
-//        // <QueryItemsAsync>
 //        /// <summary>
-//        /// Run a query (using Azure Cosmos DB SQL syntax) against the container
-//        /// Including the partition key value of lastName in the WHERE filter results in a more efficient query
+//        /// コンテナに対して（Azure Cosmos DB SQL 構文を使用して）クエリを実行する パーティションキー値 lastName を WHERE フィルタに含めると、より効率的なクエリが実行される。
 //        /// </summary>
 //        private async Task QueryItemsAsync()
 //        {
@@ -244,10 +230,10 @@
 
 //            Console.WriteLine("Running query: {0}\n", sqlQueryText);
 
-//            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
-//            FeedIterator<Family> queryResultSetIterator = container.GetItemQueryIterator<Family>(queryDefinition);
+//            var queryDefinition = new QueryDefinition(sqlQueryText);    // クエリ定義を作成
+//            FeedIterator<Family> queryResultSetIterator = container.GetItemQueryIterator<Family>(queryDefinition);  // 実際に検索
 
-//            List<Family> families = new List<Family>();
+//            List<Family> families = []; // ここに結果を格納
 
 //            while (queryResultSetIterator.HasMoreResults)
 //            {
@@ -259,46 +245,40 @@
 //                }
 //            }
 //        }
-//        // </QueryItemsAsync>
 
-//        // <ReplaceFamilyItemAsync>
 //        /// <summary>
-//        /// Replace an item in the container
+//        /// コンテナ内のアイテムを置き換える（更新する）
 //        /// </summary>
 //        private async Task ReplaceFamilyItemAsync()
 //        {
-//            ItemResponse<Family> wakefieldFamilyResponse = await container.ReadItemAsync<Family>("Wakefield.7", new PartitionKey("Wakefield"));
+//            ItemResponse<Family> wakefieldFamilyResponse = await container.ReadItemAsync<Family>("Wakefield.7", new PartitionKey("Wakefield"));     // IDとパーティションキーを指定してアイテムを読み取る
 //            var itemBody = wakefieldFamilyResponse.Resource;
 
-//            // update registration status from false to true
+//            // 登録ステータスをfalseからtrueに更新
 //            itemBody.IsRegistered = true;
-//            // update grade of child
+//            // 子供の学年を更新する
 //            itemBody.Children[0].Grade = 6;
 
-//            // replace the item with the updated content
+//            // 更新された内容で項目を置き換える
 //            wakefieldFamilyResponse = await container.ReplaceItemAsync(itemBody, itemBody.Id, new PartitionKey(itemBody.PartitionKey));
 //            Console.WriteLine("Updated Family [{0},{1}].\n \tBody is now: {2}\n", itemBody.LastName, itemBody.Id, wakefieldFamilyResponse.Resource);
 //        }
-//        // </ReplaceFamilyItemAsync>
 
-//        // <DeleteFamilyItemAsync>
 //        /// <summary>
-//        /// Delete an item in the container
+//        /// コンテナ内の項目を削除する
 //        /// </summary>
 //        private async Task DeleteFamilyItemAsync()
 //        {
 //            var partitionKeyValue = "Wakefield";
 //            var familyId = "Wakefield.7";
 
-//            // Delete an item. Note we must provide the partition key value and id of the item to delete
+//            // アイテムを削除する。削除するアイテムのパーティションキーの値とidを指定しなければならないことに注意してください。
 //            ItemResponse<Family> wakefieldFamilyResponse = await container.DeleteItemAsync<Family>(familyId, new PartitionKey(partitionKeyValue));
 //            Console.WriteLine("Deleted Family [{0},{1}]\n", partitionKeyValue, familyId);
 //        }
-//        // </DeleteFamilyItemAsync>
 
-//        // <DeleteDatabaseAndCleanupAsync>
 //        /// <summary>
-//        /// Delete the database and dispose of the Cosmos Client instance
+//        /// データベースを削除し、Cosmosクライアントインスタンスを破棄します。
 //        /// </summary>
 //        private async Task DeleteDatabaseAndCleanupAsync()
 //        {
@@ -307,9 +287,9 @@
 
 //            Console.WriteLine("Deleted Database: {0}\n", databaseId);
 
-//            //Dispose of CosmosClient
+//            // CosmosClientの破棄
 //            cosmosClient.Dispose();
 //        }
-//        // </DeleteDatabaseAndCleanupAsync>
+
 //    }
 //}
